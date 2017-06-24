573f102515fdb947dd8cfccef22dae1b2ef6b80e
andy_pi <info@andypi.co.uk>
2017-06-24 06:53:40 +0000

    switch to mailgun

--- receivesms.py
+++ receivesms.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-#sudo python /home/pi/rpi-sms-gateway/tr.py IN20170624_124709_00_10086_00.txt IN20170624_124709_00_10086_01.txt
+
 from __future__ import print_function
-import os, sys, re
-from sparkpost import SparkPost
+import os, sys, re, requests
 from config import *
-sp = SparkPost('SPARKPOST_API_KEY')
 
 def get_message():
     files = [os.path.join('/var/spool/gammu/inbox/', m) for m in sys.argv[1:]]
@@ -19,12 +17,12 @@ def get_message():
         text = text.decode('UTF-8', 'replace')
     return number, text
 
-number, text= get_message()
- 
-response = sp.transmissions.send(
-    use_sandbox=True,
-    recipients=['info@andypi.co.uk'],
-    html=text,
-    from_email='sms.andypi.co.uk',
-    subject=number
-)
+number, text = get_message()
+
+request = requests.post(mailgun_request_url, auth=('api', mailgun_api_key), data={
+    'from': mailgun_user,
+    'to': ['info@andypi.co.uk'],
+    'subject': number,
+    'text': text
+})
+
